c39e7ba83b0a26980e5251a38c8d4adc0d329785
Elena <lekolupaeva1201@gmail.com>
2023-02-06 22:36:07 +0200

    fix brain-prime game

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -5,6 +5,9 @@ import { randomizer } from '../utils.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
   for (let i = 2; i < number / 2; i += 1) {
     if (!(number % i)) {
       return false;
